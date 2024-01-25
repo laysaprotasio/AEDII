97ea253bab722cc416c0d023c1fd22d3fdc14fc2
Laysa Protásio <117881941+laysaprotasio@users.noreply.github.com>
2024-01-24 21:13:38 -0300

    Update arvore_avl_main.c

--- AVL/arvore_avl_main.c
+++ AVL/arvore_avl_main.c
@@ -8,16 +8,21 @@ int main()
     arvore a = NULL;
     int b = 1;
     
+    a = inserir_avl(a, 20, &b);
     a = inserir_avl(a, 10, &b);
     a = inserir_avl(a, 15, &b);
-    a = inserir_avl(a, 20, &b);
-    a = inserir_avl(a, 5, &b);
-    a = inserir_avl(a, 8, &b);
-    a = inserir_avl(a, 25, &b);
-    a = inserir_avl(a, 22, &b);
-    a = inserir_avl(a, 24, &b);
+    a = inserir_avl(a, 9, &b);
+    a = inserir_avl(a, 18, &b);
+    a = inserir_avl(a, 30, &b);
+    a = inserir_avl(a, 40, &b);
+    a = inserir_avl(a, 7, &b);
     a = inserir_avl(a, 26, &b);
-    a = inserir_avl(a, 23, &b);
+    a = inserir_avl(a, 28, &b);
+    a = inserir_avl(a, 50, &b);
+    a = inserir_avl(a, 12, &b);
+
+    a = remover_avl(a, 28, &b);
+    
     
 
     pre_order_avl(a);
